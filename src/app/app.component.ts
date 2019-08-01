e62a91351f93a382b7650ba05225458be41fc587
Souvik Dey <Souvik.Dey@thomsonreuters.com>
2019-08-01 17:02:39 +0530

    Code Clean Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit, AfterViewInit, AfterContentInit {
+export class AppComponent implements OnInit {
   model: any;
   loading: boolean;
-  imgSrc;
-  title = 'angular-ml';
-  predictions;
+  imgSrc: any;
+  predictions: any[] ;
   constructor(private elRef:ElementRef) { }
   async  ngOnInit() {
     this.loading = true;
@@ -19,32 +18,18 @@ export class AppComponent implements OnInit, AfterViewInit, AfterContentInit {
   }
   async fileChange(event) {
     const file = event.target.files[0];
-    console.log(file);
     if (file) {
       const reader = new FileReader();
-
       reader.readAsDataURL(file);
-
       reader.onload = (res: any) => {
-        console.log(res)
         this.imgSrc = res.target.result;
         this.predictions = [];
       };
     }
   }
-  ngAfterViewInit() {
-    // var div = this.elRef.nativeElement.querySelector('.ml-img');
-    // console.log(div);
-  }
-  ngAfterContentInit() {
-    // var div = this.elRef.nativeElement.querySelector('div');
-    // console.log(div);
-  }
   predict(){
-    console.log('Predict Clicked');
     setTimeout(async () => {
       this.predictions = await this.model.classify(this.elRef.nativeElement.querySelector('.ml-img'));
-      console.log(this.predictions);
     });
   }
 }
